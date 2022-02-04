f7ce5df10f32ec571cb0c5e4fec361fe0f00ed6e
Vineeth Venudasan <vineethv@thoughtworks.com>
2022-02-05 00:46:08 +0100

    First Code Commit

---
+++ src/main/kotlin/com/kotlin/resiliency/ResiliencyApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.resiliency
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResiliencyApplication
+
+fun main(args: Array<String>) {
+	runApplication<ResiliencyApplication>(*args)
+}
---
+++ src/test/kotlin/com/kotlin/resiliency/ResiliencyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.resiliency
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ResiliencyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
