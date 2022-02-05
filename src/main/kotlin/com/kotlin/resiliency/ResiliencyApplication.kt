d34e8baffab1af91369d051ca522c0282694e283
Vineeth Venudasan <vineethv@thoughtworks.com>
2022-02-05 12:06:47 +0100

    Adding a simple endpoint

--- src/main/kotlin/com/kotlin/resiliency/ResiliencyApplication.kt
+++ src/main/kotlin/com/kotlin/resiliency/ResiliencyApplication.kt
@@ -2,10 +2,22 @@ package com.kotlin.resiliency
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 
 @SpringBootApplication
 class ResiliencyApplication
 
 fun main(args: Array<String>) {
 	runApplication<ResiliencyApplication>(*args)
+}
+
+@Controller
+class Endpoint {
+
+	@GetMapping("/run")
+	fun run() : ResponseEntity<String> {
+		return ResponseEntity.ok("Ok")
+	}
 }
--- src/test/kotlin/com/kotlin/resiliency/ResiliencyApplicationTests.kt
+++ src/test/kotlin/com/kotlin/resiliency/ResiliencyApplicationTests.kt
@@ -1,13 +1,28 @@
 package com.kotlin.resiliency
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ResiliencyApplicationTests {
 
+	@Autowired
+	private lateinit var mockMvc: MockMvc
+
 	@Test
-	fun contextLoads() {
+	fun getsOk() {
+		mockMvc.perform(
+			get("/run")
+		).andExpect(
+			MockMvcResultMatchers.status().isOk
+		)
 	}
+
 
 }
